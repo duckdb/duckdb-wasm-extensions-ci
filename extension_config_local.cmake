5abd288178ddd587683e5b50054c61876e5ff153
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-04-18 15:37:52 +0200

    X

--- extension_config_local.cmake
+++ extension_config_local.cmake
@@ -5,11 +5,6 @@
 #   EXTENSION_CONFIGS=.github/config/out_of_tree_extensions.cmake make
 #
 
-duckdb_extension_load(httpfs
-	DONT_LINK
-	LINKED_LIBS "../../vcpkg_installed/wasm32-emscripten/lib/libcrypto.a ../../vcpkg_installed/wasm32-emscripten/lib/libssl.a"
-	)
-
 ############### SPATIAL
 
     duckdb_extension_load(spatial
