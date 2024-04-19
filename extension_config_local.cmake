67bf7d7f2df9c6ba36689a4aba911b1a6b92c514
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-04-19 13:51:51 +0200

    All but spatial

--- extension_config_local.cmake
+++ extension_config_local.cmake
@@ -6,7 +6,6 @@
 #
 
 ############### SPATIAL
-
     duckdb_extension_load(spatial
             DONT_LINK LOAD_TESTS
             GIT_URL https://github.com/carlopi/duckdb_spatial.git
@@ -15,28 +14,3 @@
             TEST_DIR test/sql
             LINKED_LIBS "../../deps/local/lib/*.a"
            )
-
-################# SQLITE_SCANNER
-duckdb_extension_load(sqlite_scanner
-        DONT_LINK LOAD_TESTS
-        GIT_URL https://github.com/duckdb/sqlite_scanner
-        GIT_TAG 091197efb34579c7195afa43dfb5925023c915c0
-        )
-
-################# SUBSTRAIT
-duckdb_extension_load(substrait
-            LOAD_TESTS DONT_LINK
-            GIT_URL https://github.com/duckdb/substrait
-            GIT_TAG 1116fb580edd3e26e675436dbdbdf4a0aa5e456e
-           )
-
-duckdb_extension_load(vss
-       LOAD_TESTS DONT_LINK
-       GIT_URL https://github.com/duckdb/duckdb_vss
-       GIT_TAG 8145f41d97178e82bed3376215eb8d02bcf1eec5
-)
-    duckdb_extension_load(azure
-            LOAD_TESTS
-            GIT_URL https://github.com/duckdb/duckdb_azure
-            GIT_TAG 09623777a366572bfb8fa53e47acdf72133a360e
-            )
