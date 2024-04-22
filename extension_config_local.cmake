7a7f3f04ef54981fae75ac14fb119819d0505af5
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-04-22 10:40:57 +0200

    only httpfs

--- extension_config_local.cmake
+++ extension_config_local.cmake
@@ -5,12 +5,7 @@
 #   EXTENSION_CONFIGS=.github/config/out_of_tree_extensions.cmake make
 #
 
-############### SPATIAL
-    duckdb_extension_load(spatial
-            DONT_LINK LOAD_TESTS
-            GIT_URL https://github.com/carlopi/duckdb_spatial.git
-            GIT_TAG main
-            INCLUDE_DIR spatial/include
-            TEST_DIR test/sql
-            LINKED_LIBS "../../deps/local/lib/*.a"
-           )
+duckdb_extension_load(httpfs
+	DONT_LINK
+	LINKED_LIBS "../../third_party/mbedtls/libduckdb_mbedtls.a;../../vcpkg_installed/wasm32-emscripten/lib/libcrypto.a;../../vcpkg_installed/wasm32-emscripten/lib/libssl.a"
+	)
