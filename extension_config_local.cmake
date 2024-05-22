a89ffc1007ba9771fafe150707d082c8a7e0eeab
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-05-22 09:28:02 +0200

    Add more to config

--- extension_config_local.cmake
+++ extension_config_local.cmake
@@ -5,16 +5,43 @@
 #   EXTENSION_CONFIGS=.github/config/out_of_tree_extensions.cmake make
 #
 
-duckdb_extension_load(autocomplete
-	DONT_LINK
-	)
 
-################# SPATIAL
-duckdb_extension_load(spatial
-    DONT_LINK LOAD_TESTS
-    GIT_URL https://github.com/carlopi/duckdb_spatial.git
-    GIT_TAG 71d24654b0fff5593b69d36878b9b8777f023d79
-    INCLUDE_DIR spatial/include
-    TEST_DIR test/sql
-    LINKED_LIBS "../../deps/local/lib/*.a"
-    )
+duckdb_extension_load(json DONT_LINK)
+duckdb_extension_load(icu DONT_LINK)
+duckdb_extension_load(parquet DONT_LINK)
+
+################# SQLITE_SCANNER
+duckdb_extension_load(sqlite_scanner
+        DONT_LINK LOAD_TESTS
+        GIT_URL https://github.com/duckdb/sqlite_scanner
+        GIT_TAG main
+        )
+
+################# SUBSTRAIT
+duckdb_extension_load(substrait
+            LOAD_TESTS DONT_LINK
+            GIT_URL https://github.com/duckdb/substrait
+            GIT_TAG main
+           )
+
+duckdb_extension_load(vss
+       LOAD_TESTS DONT_LINK
+       GIT_URL https://github.com/duckdb/duckdb_vss
+       GIT_TAG main
+)
+    duckdb_extension_load(azure
+            LOAD_TESTS
+            GIT_URL https://github.com/duckdb/duckdb_azure
+            GIT_TAG main
+            )
+
+#
+################## SPATIAL
+#duckdb_extension_load(spatial
+#    DONT_LINK LOAD_TESTS
+#    GIT_URL https://github.com/carlopi/duckdb_spatial.git
+#    GIT_TAG 71d24654b0fff5593b69d36878b9b8777f023d79
+#    INCLUDE_DIR spatial/include
+#    TEST_DIR test/sql
+#    LINKED_LIBS "../../deps/local/lib/*.a"
+#    )
