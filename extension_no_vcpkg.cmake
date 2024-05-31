9ec2367a60b3a8e8eb2efdeec2c4844f60bdb9e9
Carlo Piovesan <piovesan.carlo@gmail.com>
2024-05-31 14:44:20 +0200

    Bump

--- extension_no_vcpkg.cmake
+++ extension_no_vcpkg.cmake
@@ -2,9 +2,9 @@
 duckdb_extension_load(spatial
     DONT_LINK LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb_spatial.git
-    GIT_TAG 6826755ec32aea77777b136cc81683fa9be4df7b
+    GIT_TAG 9cf8b2915c28b6139e8573cd64869941f598541c
     INCLUDE_DIR spatial/include
     TEST_DIR test/sql
-   LINKED_LIBS "../../deps/local/lib/*.a"
+    LINKED_LIBS "../../deps/local/lib/*.a"
     )
 
